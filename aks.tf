c49cee7a385cd15d8810161ab4ed334dd025fda3
Gautamram123 <gautamnayak1050@gmail.com>
2025-05-15 14:49:30 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -2,6 +2,8 @@ provider "azurerm" {
   features {}
     subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"
     resource_provider_registrations = "none"
+    use_msi = false
+    use_cli = false
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
