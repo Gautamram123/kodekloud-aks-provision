8ea022ec50670088fb429ef9ea573588c4106768
Gautamram123 <gautamnayak1050@gmail.com>
2025-05-15 14:55:16 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,21 +1,27 @@
+variable "subscription_id" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "dns_prefix" { type = string }
+variable "node_count" { type = number }
+variable "vm_size" { type = string }
+
 provider "azurerm" {
   features {}
-    subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"
-    resource_provider_registrations = "none"
-    use_msi = false
-    use_cli = false
+  subscription_id = var.subscription_id
+  use_cli         = false
+  use_msi         = false
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-minimal-cluster"
-  location            = "East US"                      # Use the location of your existing RG
-  resource_group_name = "kml_rg_main-69aeb635f1804be9" # Replace with your RG name
-  dns_prefix          = "aksmincluster"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2s_v3"
+    node_count = var.node_count
+    vm_size    = var.vm_size
   }
 
   identity {
